e00f173dba17c981d2f29dfa0d36ba0d432c5f12
吴思伟 <18683789594@163.com>
2022-06-10 11:23:28 +0800

    commit

--- src/main/java/com/framework/cloud/gateway/GatewayApplication.java
+++ src/main/java/com/framework/cloud/gateway/GatewayApplication.java
@@ -1,6 +1,5 @@
 package com.framework.cloud.gateway;
 
-import com.framework.cloud.common.utils.GsonUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        String rt = "{ \"corpId\": \"150000000\" }";
-        T1 t1 = GsonUtil.toJavaBean(rt, T1.class);
         SpringApplication.run(GatewayApplication.class, args);
     }
 
